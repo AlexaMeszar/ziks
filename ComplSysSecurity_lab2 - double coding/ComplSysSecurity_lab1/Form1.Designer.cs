3487f5d7cc3957397e733f60e04c5b71db8ba9cf
AlexaMeszar <50546820+AlexaMeszar@users.noreply.github.com>
2021-04-04 16:27:08 +0200

    Add files via upload

---
+++ ComplSysSecurity_lab2 - double coding/ComplSysSecurity_lab1/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ComplSysSecurity_lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.originalTXT = new System.Windows.Forms.TextBox();
+            this.decodedTXT = new System.Windows.Forms.TextBox();
+            this.codedTXT = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.keyBox = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.keyBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // originalTXT
+            // 
+            this.originalTXT.Location = new System.Drawing.Point(55, 34);
+            this.originalTXT.Multiline = true;
+            this.originalTXT.Name = "originalTXT";
+            this.originalTXT.Size = new System.Drawing.Size(222, 549);
+            this.originalTXT.TabIndex = 0;
+            // 
+            // decodedTXT
+            // 
+            this.decodedTXT.Location = new System.Drawing.Point(505, 34);
+            this.decodedTXT.Multiline = true;
+            this.decodedTXT.Name = "decodedTXT";
+            this.decodedTXT.Size = new System.Drawing.Size(210, 549);
+            this.decodedTXT.TabIndex = 1;
+            // 
+            // codedTXT
+            // 
+            this.codedTXT.Location = new System.Drawing.Point(283, 34);
+            this.codedTXT.Multiline = true;
+            this.codedTXT.Name = "codedTXT";
+            this.codedTXT.Size = new System.Drawing.Size(216, 549);
+            this.codedTXT.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(767, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(203, 41);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Code";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(993, 87);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(202, 41);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Decode";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Original Text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(279, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Coded Text";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(501, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Decoded Text";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(763, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Key";
+            // 
+            // keyBox
+            // 
+            this.keyBox.Location = new System.Drawing.Point(804, 32);
+            this.keyBox.Name = "keyBox";
+            this.keyBox.Size = new System.Drawing.Size(120, 26);
+            this.keyBox.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1207, 653);
+            this.Controls.Add(this.keyBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.codedTXT);
+            this.Controls.Add(this.decodedTXT);
+            this.Controls.Add(this.originalTXT);
+            this.Name = "Form1";
+            this.Text = "Lab 2";
+            ((System.ComponentModel.ISupportInitialize)(this.keyBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox originalTXT;
+        private System.Windows.Forms.TextBox decodedTXT;
+        private System.Windows.Forms.TextBox codedTXT;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown keyBox;
+    }
+}
+
---
+++ ComplSysSecurity_lab2 - double coding/ComplSysSecurity_lab1/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComplSysSecurity_lab1
+{
+    public partial class Form1 : Form
+    {
+        public char[] alphabet;
+
+        public Form1()
+        {
+            InitializeComponent();
+            alphabet = Enumerable.Range('a', 'z' - 'a' + 1).Select(i => (Char)i).ToArray(); 
+        }
+
+        private string codeText(string text)
+        {
+            string[] codedStrArr = new string[text.Length];
+            string[] strArr = text.ToCharArray().Select(c => c.ToString()).ToArray();
+            int key = Convert.ToInt32(keyBox.Value);
+            char codedC;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char c = Convert.ToChar(strArr[i]);
+                if ((c == ' ') || (c == ',' ) || (c == '.'))
+                {
+                    codedC = c;
+                }
+                else
+                {
+                    Tuple<int, bool> tuple = getCharPos(c);
+                    if (tuple.Item2 == false)
+                    {
+                        MessageBox.Show("Error occured!");
+                        break;
+                    }
+                    codedC = getCharForIndex((tuple.Item1 + key * 2));
+                }             
+                
+                codedStrArr[i] = codedC.ToString();
+            }
+
+            string codedText = string.Join("", codedStrArr);
+
+            return codedText;
+        }
+
+        private string decodeText(string text)
+        {
+            string[] decodedStrArr = new string[text.Length];
+            string[] strArr = text.ToCharArray().Select(c => c.ToString()).ToArray();
+            int key = Convert.ToInt32(keyBox.Value);
+            char decodedC;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char c = Convert.ToChar(strArr[i]);
+                if ((c == ' ') || (c == ',') || (c == '.'))
+                {
+                    decodedC = c;
+                }
+                else
+                {
+                    Tuple<int, bool> tuple = getCharPos(c);
+                    if (tuple.Item2 == false)
+                    {
+                        MessageBox.Show("Error occured!");
+                        break;
+                    }
+
+                    int y = (tuple.Item1 - key);
+
+                    if (y < 0)
+                    {
+                        decodedC = getCharForIndex(alphabet.Count() + y);
+                    }
+                    else
+                    {
+                        decodedC = getCharForIndex((tuple.Item1 - key * 2));
+                    }                    
+                }
+
+                decodedStrArr[i] = decodedC.ToString();
+            }
+
+            string decodedText = string.Join("", decodedStrArr);
+
+            return decodedText;
+        }
+
+        private Tuple<int, bool> getCharPos(char c)
+        {
+            int index = 0;
+            bool found = false;
+            for (int i = 0; i < alphabet.Count(); i++)
+            {
+                char compare = alphabet[i];
+                if (compare == char.ToLower(c))
+                {
+                    index = i;
+                    found = true;
+                }
+            }
+            return Tuple.Create(index, found);
+        }
+
+        private char getCharForIndex(int index)
+        {
+            int x = index % alphabet.Count();
+            char c = alphabet[x];
+            return c; 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = originalTXT.Text;
+            codedTXT.Text = codeText(text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string text = codedTXT.Text;
+            decodedTXT.Text = decodeText(text);
+        }
+    }
+}
